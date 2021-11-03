b5e03614cf7dcb5f831b27905771bce207853bbd
Weymarcv <89791568+WeimarC@users.noreply.github.com>
2021-11-03 16:46:55 -0300

    Parte Diez Animaciones

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import Header from "./components/layout/Header";
 import Main from "./components/layout/Main";
 import Footer from "./components/layout/Footer";
 import Home from "./src/home/Home";
-import Contactanos from "./contactanos/Contactanos";
+/* import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"; */
+/* import Contactanos from "./contactanos/Contactanos"; */
 
 function App() {
   return (
     <>
       <Header />
       <Main>
-        <Contactanos />
-        <Home />
+     {/*  <Contactanos /> */}
+      <Home />
       </Main>
       <Footer />
     </>
--- src/src/home/Tarjeta.jsx
+++ src/src/home/Tarjeta.jsx
@@ -2,24 +2,30 @@ import React, { useState } from "react";
 import "./Tarjeta.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShareAlt } from "@fortawesome/free-solid-svg-icons";
+import { render } from "react-dom";
+import { motion } from "framer-motion";
 
 const Tarjeta = ({ imagen, fotoDePerfil, NombreDelHaiku, haiku}) => {
   const [invisible, setInvisible] = useState(true);
 
 
+
   const cambiarVisibilidad = () => {
-    if( invisible == true  ) {
-      setInvisible = true;
-    }else if( setInvisible == false ) {
-      invisible = false;
+    if( invisible == false  ) {
+      setInvisible(true);
+    }else if( invisible == true ) {
+      setInvisible(false);
     }
   }
 
   return (
     <div>
+      <div className="example-container">
+      <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} />
+    </div>
       <div className="imagenYHaiku" onClick={cambiarVisibilidad}>
       <p className="Haiku" style={{ display: invisible ? "none" : "block" }}> {haiku} </p>
-      <img src={imagen} alt="" className="imagenP" />
+      <img src={imagen} alt="/" className="imagenP" />
       </div>
       <div className="cuerpoTarjeta">
         <div className="fotoYTituloH">
